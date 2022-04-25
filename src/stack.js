c1d42ac642c3b6a4c9768972e2beea8ce248be7c
IDemidova <irina.demidova.irina@gmail.com>
2022-04-25 11:42:24 +0300

    feat: add solution for stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return parseInt(this.stack.splice(this.stack.length - 1, 1));
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return parseInt(this.stack[this.stack.length - 1]);
   }
 }
 
